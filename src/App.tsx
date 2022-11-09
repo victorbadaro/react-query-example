dc374c161c8cb8b8f59dee9b1c2b7f2ad7eb4759
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2022-11-08 22:37:21 -0300

    feat: add request to get github repos in the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from 'react';
+import { api } from './services/api';
 import './styles/global.css';
 
+interface Repo {
+  id: number;
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 export function App() {
-  return <h1>Hello</h1>;
+  const [repos, setRepos] = useState<Repo[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await api.get<Repo[]>('/users/victorbadaro/repos');
+
+      setRepos(data);
+    })();
+  }, []);
+
+  return (
+    <>
+      <h1>Repos</h1>
+      {repos.length > 0 ? (
+        <ul>
+          {repos.map(repo => (
+            <li key={repo.id}>
+              <a href={repo.html_url} target="_blank">{repo.name}</a> - <span>{repo.description}</span>
+            </li>
+          ))}
+        </ul>
+      ) : null}
+    </>
+  );
 }
